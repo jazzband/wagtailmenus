1d64138d2ab7468709caf6525c77ee8947cf7c6c
Andy Babic <ababic@rkh.co.uk>
2016-04-23 18:40:43 +0100

    Update version to 1.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(),
     license="MIT",
     keywords="wagtail cms model utility",
-    download_url="https://github.com/rkhleics/wagtailmenus/tarball/1.0.8",
+    download_url="https://github.com/rkhleics/wagtailmenus/tarball/1.1.0",
     url="https://github.com/rkhleics/wagtailmenus",
     include_package_data=True,
     zip_safe=False,
