592532b69ccfa3d5e13d12834a25bd4eec913c51
Andy Babic <ababic@rkh.co.uk>
2016-05-12 09:28:30 +0100

    Ditch unnecessary urls from test urls

--- wagtailmenus/tests/urls.py
+++ wagtailmenus/tests/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.conf.urls import include, url
 
 from django.views.generic import TemplateView
@@ -10,11 +9,3 @@ urlpatterns = [
     url(r'^custom-url/$', TemplateView.as_view(template_name='page.html')),
     url(r'', include(wagtail_urls)),
 ]
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
