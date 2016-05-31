daae0a4739f16b60f227e12be2daf03cddbdfe5c
Andy Babic <ababic@rkh.co.uk>
2016-05-31 15:02:11 +0100

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(),
     license="MIT",
     keywords="wagtail cms model utility",
-    download_url="https://github.com/rkhleics/wagtailmenus/tarball/1.2.0",
+    download_url="https://github.com/rkhleics/wagtailmenus/tarball/1.2.1",
     url="https://github.com/rkhleics/wagtailmenus",
     include_package_data=True,
     zip_safe=False,
