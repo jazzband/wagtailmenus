4d30c1b3b475debefc2b6b0226e1fc62eb1f1bfa
Andy Babic <ababic@rkh.co.uk>
2016-10-18 10:49:40 +0100

    Replace binary strings with normal strings in an older migration

--- wagtailmenus/migrations/0011_auto_20160415_1519.py
+++ wagtailmenus/migrations/0011_auto_20160415_1519.py
@@ -14,11 +14,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='flatmenuitem',
             name='url_append',
-            field=models.CharField(help_text=b"Use this to optionally append a #hash or querystring to the above page's URL.", max_length=255, verbose_name='Append to URL', blank=True),
+            field=models.CharField(help_text="Use this to optionally append a #hash or querystring to the above page's URL.", max_length=255, verbose_name='Append to URL', blank=True),
         ),
         migrations.AlterField(
             model_name='mainmenuitem',
             name='url_append',
-            field=models.CharField(help_text=b"Use this to optionally append a #hash or querystring to the above page's URL.", max_length=255, verbose_name='Append to URL', blank=True),
+            field=models.CharField(help_text="Use this to optionally append a #hash or querystring to the above page's URL.", max_length=255, verbose_name='Append to URL', blank=True),
         ),
     ]
