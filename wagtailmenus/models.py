4ac7d8d80ff77756cb861c3dc9b2344a2e7bf66e
ak <ak@keysystems.ru>
2016-10-19 12:25:04 +0300

    Must explicitly specify verbose_name in order to be able to localize form fields labels

--- wagtailmenus/models.py
+++ wagtailmenus/models.py
@@ -106,11 +106,13 @@ class MenuItem(models.Model):
         null=True,
     )
     link_text = models.CharField(
+        verbose_name=_('link text'),
         max_length=255,
         blank=True,
         help_text=_("Must be set if you wish to link to a custom URL."),
     )
     handle = models.CharField(
+        verbose_name=_('handle'),
         max_length=100,
         blank=True,
         help_text=_(
@@ -206,16 +208,20 @@ class MainMenu(ClusterableModel):
 class FlatMenu(ClusterableModel):
     site = models.ForeignKey(
         'wagtailcore.Site',
+        verbose_name=_('site'),
         related_name="flat_menus")
     title = models.CharField(
+        verbose_name=_('title'),
         max_length=255,
         help_text=_("For internal reference only."))
     handle = models.SlugField(
+        verbose_name=_('handle'),
         max_length=100,
         help_text=_(
             "Used to reference this menu in templates etc. Must be unique "
             "for the selected site."))
     heading = models.CharField(
+        verbose_name=_('heading'),
         max_length=255,
         blank=True,
         help_text=_(
