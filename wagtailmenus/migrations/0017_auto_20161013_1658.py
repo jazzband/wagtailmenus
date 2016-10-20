708e32f64280eb95a9ca0be3813aa434d88ec6c6
Andy Babic <ababic@rkh.co.uk>
2016-10-20 09:02:34 +0100

    Fix migration that broke during rebase

--- wagtailmenus/migrations/0017_auto_20161013_1658.py
+++ wagtailmenus/migrations/0017_auto_20161013_1658.py
@@ -12,7 +12,17 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-         migrations.AddField(
+        migrations.AddField(
+            model_name='flatmenu',
+            name='max_levels',
+            field=models.PositiveSmallIntegerField(choices=[(1, '1: No sub-navigation (flat)'), (2, '2: Allow 1 level of sub-navigation'), (3, '3: Allow 2 levels of sub-navigation'), (4, '4: Allow 3 levels of sub-navigation')], default=1, help_text='The default number of maximum levels to display when rendering this menu. The value can be overidden by supplying a different `max_levels` value to the `flat_menu` tag.', verbose_name='maximum levels'),
+        ),
+        migrations.AddField(
+            model_name='mainmenu',
+            name='max_levels',
+            field=models.PositiveSmallIntegerField(choices=[(1, '1: No sub-navigation (flat)'), (2, '2: Allow 1 level of sub-navigation'), (3, '3: Allow 2 levels of sub-navigation'), (4, '4: Allow 3 levels of sub-navigation')], default=2, help_text='The default number of maximum levels to display when rendering this menu. The value can be overidden by supplying a different `max_levels` value to the `main_menu` tag.', verbose_name='maximum levels'),
+        ),
+        migrations.AddField(
             model_name='flatmenu',
             name='use_specific',
             field=models.PositiveSmallIntegerField(choices=[(0, 'OFF (Most efficient)'), (1, 'AUTO'), (2, 'TOP-LEVEL'), (3, 'ALWAYS (Least efficient)')], default=0, help_text="NOTE: Some menu functionality may be dependant on this setting. Please only change if you know what you're doing.", verbose_name='specific page usage'),
@@ -21,15 +31,5 @@ class Migration(migrations.Migration):
             model_name='mainmenu',
             name='use_specific',
             field=models.PositiveSmallIntegerField(choices=[(0, 'OFF (Most efficient)'), (1, 'AUTO'), (2, 'TOP-LEVEL'), (3, 'ALWAYS (Least efficient)')], default=1, help_text="NOTE: Some menu functionality may be dependant on this setting. Please only change if you know what you're doing.", verbose_name='specific page usage'),
-        ),
-        migrations.AlterField(
-            model_name='flatmenu',
-            name='max_levels',
-            field=models.PositiveSmallIntegerField(choices=[(1, '1: No sub-navigation (flat)'), (2, '2: Allow 1 level of sub-navigation'), (3, '3: Allow 2 levels of sub-navigation'), (4, '4: Allow 3 levels of sub-navigation')], default=1, help_text='The default number of maximum levels to display when rendering this menu. The value can be overidden by supplying a different `max_levels` value to the `flat_menu` tag.', verbose_name='maximum levels'),
-        ),
-        migrations.AlterField(
-            model_name='mainmenu',
-            name='max_levels',
-            field=models.PositiveSmallIntegerField(choices=[(1, '1: No sub-navigation (flat)'), (2, '2: Allow 1 level of sub-navigation'), (3, '3: Allow 2 levels of sub-navigation'), (4, '4: Allow 3 levels of sub-navigation')], default=2, help_text='The default number of maximum levels to display when rendering this menu. The value can be overidden by supplying a different `max_levels` value to the `main_menu` tag.', verbose_name='maximum levels'),
         ),
     ]
