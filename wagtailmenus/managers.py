4d0029eac96e000d091f79ee3af55403a5679f37
Andy Babic <ababic@rkh.co.uk>
2016-10-21 00:15:55 +0100

    Ditch unused manager methods

--- wagtailmenus/managers.py
+++ wagtailmenus/managers.py
@@ -13,12 +13,3 @@ class MenuItemManager(models.Manager):
             models.Q(link_page__expired=False) &
             models.Q(link_page__show_in_menus=True)
         )
-
-    def page_links(self):
-        return self.filter(link_page__isnull=False)
-
-    def page_links_for_display(self):
-        return self.page_links().filter(
-            link_page__live=True,
-            link_page__expired=False,
-            link_page__show_in_menus=True)
