9ad5b70e5e384adb3d6811abfaa529163faddede
Andy Babic <ababic@rkh.co.uk>
2017-05-15 09:05:19 +0100

    Update test page models to accept request parameter to avoid warnings during tests

--- wagtailmenus/tests/models/pages.py
+++ wagtailmenus/tests/models/pages.py
@@ -43,28 +43,28 @@ class MultilingualMenuPage(MenuPage):
     def modify_submenu_items(
         self, menu_items, current_page, current_ancestor_ids,
         current_site, allow_repeating_parents, apply_active_classes,
-        original_menu_tag, menu_instance=None
+        original_menu_tag, menu_instance, request
     ):
         return super(MultilingualMenuPage, self).modify_submenu_items(
             menu_items, current_page, current_ancestor_ids,
             current_site, allow_repeating_parents, apply_active_classes,
-            original_menu_tag, menu_instance)
+            original_menu_tag, menu_instance, request)
 
     def has_submenu_items(
         self, current_page, allow_repeating_parents, original_menu_tag,
-        menu_instance=None
+        menu_instance, request
     ):
         return super(MultilingualMenuPage, self).has_submenu_items(
             current_page, allow_repeating_parents, original_menu_tag,
-            menu_instance)
+            menu_instance, request)
 
     def get_repeated_menu_item(
         self, current_page, current_site, apply_active_classes,
-        original_menu_tag
+        original_menu_tag, request
     ):
         item = super(MultilingualMenuPage, self).get_repeated_menu_item(
             current_page, current_site, apply_active_classes,
-            original_menu_tag)
+            original_menu_tag, request)
         item.text = self.translated_repeated_item_text or self.translated_title
         return item
 
@@ -111,7 +111,7 @@ class ContactPage(MenuPage):
     def modify_submenu_items(
         self, menu_items, current_page, current_ancestor_ids,
         current_site, allow_repeating_parents, apply_active_classes,
-        original_menu_tag, menu_instance
+        original_menu_tag, menu_instance, request
     ):
         menu_items = super(ContactPage, self).modify_submenu_items(
             menu_items, current_page, current_ancestor_ids,
@@ -144,7 +144,7 @@ class ContactPage(MenuPage):
 
     def has_submenu_items(
         self, current_page, allow_repeating_parents,
-        original_menu_tag, menu_instance
+        original_menu_tag, menu_instance, request
     ):
         """
         Because `modify_submenu_items` is being used to add additional menu
@@ -156,4 +156,4 @@ class ContactPage(MenuPage):
             return True
         return super(ContactPage, self).has_submenu_items(
             current_page, allow_repeating_parents, original_menu_tag,
-            menu_instance)
+            menu_instance, request)
