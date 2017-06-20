94ede7ed81a725d14fffa81134e36dd667370917
Andy Babic <ababic@rkh.co.uk>
2017-06-20 09:47:48 +0100

    Modify ‘runtests’ to accept a ‘deprecation’ argument that can be used to optionally surface deprecation warnings in tests

--- runtests.py
+++ runtests.py
@@ -1,15 +1,51 @@
 #!/usr/bin/env python
+import argparse
 import os
 import sys
+import warnings
 
-import django
-from django.conf import settings
-from django.test.utils import get_runner
+from django.core.management import execute_from_command_line
 
-if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'wagtailmenus.settings.testing'
-    django.setup()
-    TestRunner = get_runner(settings)
-    test_runner = TestRunner()
-    failures = test_runner.run_tests(["wagtailmenus"])
-    sys.exit(bool(failures))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wagtailmenus.settings.testing'
+
+
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--deprecation',
+        choices=['all', 'pending', 'imminent', 'none'],
+        default='imminent'
+    )
+    return parser
+
+
+def parse_args(args=None):
+    return make_parser().parse_args(args)
+
+
+def runtests():
+    args = parse_args()
+
+    if args.deprecation == 'all':
+        # Show all deprecation warnings from all packages
+        warnings.simplefilter('default', category=DeprecationWarning)
+        warnings.simplefilter('default', category=PendingDeprecationWarning)
+    elif args.deprecation == 'pending':
+        # Show all deprecation warnings
+        warnings.filterwarnings('default', category=DeprecationWarning)
+        warnings.filterwarnings('default', category=PendingDeprecationWarning)
+    elif args.deprecation == 'imminent':
+        # Show only imminent deprecation warnings
+        warnings.filterwarnings('default', category=DeprecationWarning)
+    elif args.deprecation == 'none':
+        # Deprecation warnings are ignored by default
+        pass
+
+    argv = [sys.argv[0], 'test']
+    try:
+        execute_from_command_line(argv)
+    except:
+        pass
+
+if __name__ == '__main__':
+    runtests()
