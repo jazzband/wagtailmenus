b30744558b648d2cdcb0a3c7bbbc3a1196b3c31c
tholliday <tholliday@morristechnology.com>
2017-07-10 11:36:02 -0400

    removing default argument for menu template tags, pulling value from context if it doesn't exist.

--- wagtailmenus/templatetags/menu_tags.py
+++ wagtailmenus/templatetags/menu_tags.py
@@ -24,7 +24,7 @@ register = Library()
 def main_menu(
     context, max_levels=None, use_specific=None, apply_active_classes=True,
     allow_repeating_parents=True, show_multiple_levels=True,
-    template='', sub_menu_template='', use_absolute_urls=False,
+    template='', sub_menu_template='', use_absolute_urls=None,
 ):
     validate_supplied_values('main_menu', max_levels=max_levels,
                              use_specific=use_specific)
@@ -46,6 +46,9 @@ def main_menu(
 
     if use_specific is not None:
         menu.set_use_specific(use_specific)
+
+    if use_absolute_urls is None:
+        use_absolute_urls = context.get('use_absolute_urls', False)
 
     # Identify templates for rendering
     template_names = get_template_names('main', request, template)
@@ -93,7 +96,7 @@ def flat_menu(
     allow_repeating_parents=True, show_multiple_levels=True,
     template='', sub_menu_template='',
     fall_back_to_default_site_menus=flat_menus_fbtdsm,
-    use_absolute_urls=False,
+    use_absolute_urls=None,
 ):
     validate_supplied_values('flat_menu', max_levels=max_levels,
                              use_specific=use_specific)
@@ -121,6 +124,9 @@ def flat_menu(
 
     if use_specific is not None:
         menu.set_use_specific(use_specific)
+
+    if use_absolute_urls is None:
+        use_absolute_urls = context.get('use_absolute_urls', False)
 
     template_names = menu.get_template_names(request, template)
     t = context.template.engine.select_template(template_names)
@@ -236,7 +242,7 @@ def get_sub_menu_items_for_page(
 def sub_menu(
     context, menuitem_or_page, stop_at_this_level=None, use_specific=None,
     allow_repeating_parents=None, apply_active_classes=None, template='',
-    use_absolute_urls=False,
+    use_absolute_urls=None,
 ):
     """
     Retrieve the children pages for the `menuitem_or_page` provided, turn them
@@ -263,6 +269,9 @@ def sub_menu(
 
     if allow_repeating_parents is None:
         allow_repeating_parents = context.get('allow_repeating_parents', True)
+
+    if use_absolute_urls is None:
+        use_absolute_urls = context.get('use_absolute_urls', False)
 
     if not template:
         template = context.get(
@@ -323,7 +332,7 @@ def section_menu(
     max_levels=app_settings.DEFAULT_SECTION_MENU_MAX_LEVELS,
     template='', sub_menu_template='',
     use_specific=app_settings.DEFAULT_SECTION_MENU_USE_SPECIFIC,
-    use_absolute_urls=False,
+    use_absolute_urls=None,
 ):
     """Render a section menu for the current section."""
 
@@ -339,6 +348,9 @@ def section_menu(
 
     if not show_multiple_levels:
         max_levels = 1
+
+    if use_absolute_urls is None:
+        use_absolute_urls = context.get('use_absolute_urls', False)
 
     # Create a menu instance that can fetch all pages at once and return
     # for subpages for each branch as they are needed
@@ -425,7 +437,7 @@ def children_menu(
     max_levels=app_settings.DEFAULT_CHILDREN_MENU_MAX_LEVELS,
     template='', sub_menu_template='',
     use_specific=app_settings.DEFAULT_CHILDREN_MENU_USE_SPECIFIC,
-    use_absolute_urls=False,
+    use_absolute_urls=None,
 ):
     validate_supplied_values(
         'children_menu', max_levels=max_levels, use_specific=use_specific,
@@ -439,6 +451,9 @@ def children_menu(
         parent_page = context.get('self')
     if not parent_page:
         return ''
+
+    if use_absolute_urls is None:
+        use_absolute_urls = context.get('use_absolute_urls', False)
 
     # Create a menu instance that can fetch all pages at once and return
     # for subpages for each branch as they are needed
