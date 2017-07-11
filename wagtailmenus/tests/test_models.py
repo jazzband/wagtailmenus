4ee5ab4f83b512a947d70dc558f54b9a7887a34b
tholliday <tholliday@morristechnology.com>
2017-07-11 10:36:33 -0400

    removing test fixture and updated corresponding test

--- wagtailmenus/tests/test_models.py
+++ wagtailmenus/tests/test_models.py
@@ -7,7 +7,7 @@ from django.core.exceptions import ValidationError
 
 from wagtail.wagtailcore.models import Page, Site
 from wagtailmenus.models import MainMenu, MainMenuItem, FlatMenu
-from wagtailmenus.tests.models import LinkPage
+from wagtailmenus.tests.models import LinkPage, NoAbsoluteUrlsPage
 from wagtailmenus.utils.deprecation import RemovedInWagtailMenus26Warning
 
 
@@ -282,12 +282,18 @@ class TestLinkPage(TestCase):
 
 
 class TestNoAbsoluteUrlsPage(TestCase):
-    fixtures = ['updated_test.json']
+
+    def setUp(self):
+        self.site = Site.objects.select_related('root_page').get(is_default_site=True)
+        self.no_absolute_urls_page = NoAbsoluteUrlsPage(
+            title='Compatibility Test Page',
+        )
+        self.site.root_page.add_child(instance=self.no_absolute_urls_page)
 
     def test_raises_deprecation_warning(self):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter('always', RemovedInWagtailMenus26Warning)
-            self.client.get('/compatibility-test-page/')
+            self.client.get(self.no_absolute_urls_page.relative_url(self.site))
             self.assertNotEqual(len(w), 0)
             warning_messages = set(str(warning.message) for warning in w)
             # Make sure our expected warning was logged
