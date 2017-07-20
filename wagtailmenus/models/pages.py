002dac1e1f2c5c241a8af95ea80ad0c61bd01ea5
Andy Babic <ababic@rkh.co.uk>
2017-07-20 17:02:41 +0100

    Minor code tidy (fixing flake 8 formatting warnings)

--- wagtailmenus/models/pages.py
+++ wagtailmenus/models/pages.py
@@ -12,7 +12,9 @@ from django.utils.translation import ugettext_lazy as _
 from wagtail.wagtailcore.models import Page
 
 from wagtailmenus.utils.inspection import accepts_kwarg
-from wagtailmenus.utils.deprecation import RemovedInWagtailMenus25Warning, RemovedInWagtailMenus26Warning
+from wagtailmenus.utils.deprecation import (
+    RemovedInWagtailMenus25Warning, RemovedInWagtailMenus26Warning
+)
 from .. import app_settings
 from ..forms import LinkPageAdminForm
 from ..panels import menupage_settings_panels, linkpage_edit_handler
@@ -61,14 +63,14 @@ class MenuPageMixin(models.Model):
             """
 
             # Create dict of kwargs to send to `get_repeated_menu_item`
-            method_kwargs = {
+            kwargs = {
                 'current_page': current_page,
                 'current_site': current_site,
                 'apply_active_classes': apply_active_classes,
                 'original_menu_tag': original_menu_tag,
             }
             if accepts_kwarg(self.get_repeated_menu_item, 'request'):
-                method_kwargs['request'] = request
+                kwargs['request'] = request
             else:
                 msg = (
                     "The 'get_repeated_menu_item' method on '%s' should be "
@@ -79,20 +81,22 @@ class MenuPageMixin(models.Model):
                 )
                 warnings.warn(msg, RemovedInWagtailMenus25Warning)
 
-            if accepts_kwarg(self.get_repeated_menu_item, 'use_absolute_page_urls'):
-                method_kwargs['use_absolute_page_urls'] = use_absolute_page_urls
+            if accepts_kwarg(
+                self.get_repeated_menu_item, 'use_absolute_page_urls'
+            ):
+                kwargs['use_absolute_page_urls'] = use_absolute_page_urls
             else:
                 msg = (
                     "The 'get_repeated_menu_item' method on '%s' should be "
-                    "updated to accept a 'use_absolute_page_urls' keyword argument. View the "
-                    "2.4 release notes for more info: https://github.com/"
-                    "rkhleics/wagtailmenus/releases/tag/v.2.4.0" %
-                    self.__class__.__name__
+                    "updated to accept a 'use_absolute_page_urls' keyword "
+                    "argument. View the 2.4 release notes for more info: "
+                    "https://github.com/rkhleics/wagtailmenus/releases/tag/"
+                    "v.2.4.0" % self.__class__.__name__
                 )
                 warnings.warn(msg, RemovedInWagtailMenus26Warning)
 
             # Call `get_repeated_menu_item` using the above kwargs dict
-            repeated_item = self.get_repeated_menu_item(**method_kwargs)
+            repeated_item = self.get_repeated_menu_item(**kwargs)
             menu_items.insert(0, repeated_item)
         return menu_items
 
