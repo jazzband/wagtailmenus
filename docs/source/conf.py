4cf1330bab85f3c4ec9be063f31742c3a0a69d56
Andy Babic <ababic@rkh.co.uk>
2017-07-24 22:06:57 +0100

    Update theme to 'default'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -91,7 +91,7 @@ spelling_word_list_filename = 'spelling_wordlist.txt'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
