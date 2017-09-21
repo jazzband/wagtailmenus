90415a3da156b1d837d7e9fdfdec314d0e7beee3
Andy Babic <ababic@rkh.co.uk>
2017-09-21 13:28:51 +0100

    Minor formatting correction in SubMenuUsageError message

--- wagtailmenus/errors.py
+++ wagtailmenus/errors.py
@@ -6,7 +6,7 @@ class SubMenuUsageError(TemplateSyntaxError):
         return TemplateSyntaxError.__init__(self, (
             "The 'sub_menu' tag can only be used in menu templates to render "
             "parts of other menus, initiated by calling one of the other "
-            "included tags: 'main_menu','flat_menu', 'section_menu' or "
+            "included tags: 'main_menu', 'flat_menu', 'section_menu' or "
             "'children_menu' (custom tags are not supported). You might "
             "want to update your template to use the 'children_menu' tag "
             "instead."))
