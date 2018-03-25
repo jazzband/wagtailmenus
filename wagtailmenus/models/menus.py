d0da9fa0995f3afa1252f5e0e8a20c18f249276e
Andy Babic <ababic@rkh.co.uk>
2018-03-25 16:51:28 +0100

    Warn about deprecation of current default behaviour when WAGTAILMENUS_CUSTOM_URL_SMART_ACTIVE_CLASSES is not set or False

--- wagtailmenus/models/menus.py
+++ wagtailmenus/models/menus.py
@@ -1,3 +1,4 @@
+import warnings
 from collections import defaultdict, namedtuple
 from types import GeneratorType
 
@@ -21,6 +22,7 @@ from .. import app_settings
 from ..forms import FlatMenuAdminForm
 from ..panels import (
     main_menu_content_panels, flat_menu_content_panels, menu_settings_panels)
+from ..utils.deprecation import RemovedInWagtailMenus210Warning
 from ..utils.misc import get_site_from_request
 from .menuitems import MenuItem
 from .mixins import DefinesSubMenuTemplatesMixin
@@ -28,6 +30,19 @@ from .pages import AbstractLinkPage
 
 
 mark_safe_lazy = lazy(mark_safe, six.text_type)
+
+# TODO: To be removed in v.2.10
+WARNING_MSG = (
+    "The current default 'active' class attribution behaviour for menu items "
+    "that link to custom URLs is deprecated in favour of the smarter approach "
+    "introduced in 2.8. You can use this new behaviour right now by "
+    "adding 'WAGTAILMENUS_CUSTOM_URL_SMART_ACTIVE_CLASSES = True' "
+    "to your project's settings (which will also silence this warning). See "
+    "the 2.8 release notes for more info: "
+    "http://wagtailmenus.readthedocs.io/en/stable/releases/2.8.0.html"
+)
+if not app_settings.CUSTOM_URL_SMART_ACTIVE_CLASSES:
+    warnings.warn(WARNING_MSG, category=RemovedInWagtailMenus210Warning)
 
 
 ContextualVals = namedtuple('ContextualVals', (
