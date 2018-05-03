e487181c1075d6629375c488e0ccbf2df3d5e2fc
Andy Babic <ababic@rkh.co.uk>
2018-05-03 10:09:00 +0100

    Pin exact versions of test dependencies for clarity

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ branch_url = "%stree/stable/%s" % (base_url, stable_branch_name)
 
 # Testing dependencies
 testing_extras = [
-    'django-webtest>=1.8.0,<1.9.3',
-    'beautifulsoup4>=4.4.1,<4.5.02',
+    'django-webtest==1.9.2',
+    'beautifulsoup4==4.5.1',
     'coverage>=3.7.0',
     'wagtail-condensedinlinepanel==0.3',
     'mock>=2.0.0'
