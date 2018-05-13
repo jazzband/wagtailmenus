54ba7d52c4e9ee9f15fa17298e80304c3b8b82cf
Andy Babic <ababic@rkh.co.uk>
2018-05-13 21:45:45 +0100

    Improve readability of new tests, and replace 'test_no_additional_queries_made_if_the_supplied_site_is_the_default_site' with  'test_returns_provided_site_matches_over_default_site_matches'

--- wagtailmenus/tests/test_flatmenu_class.py
+++ wagtailmenus/tests/test_flatmenu_class.py
@@ -34,73 +34,54 @@ class TestGetForSite(FlatMenuTestCase):
     """Unit tests for AbstractFlatMenu.get_for_site()"""
     def setUp(self):
         super().setUp()
-        self.secondary_site = Site.objects.create(
+        self.default_site = self.site
+        self.not_default_site = Site.objects.create(
             hostname='test2.com',
             root_page=Page.objects.all().first(),
             is_default_site=False,
         )
-        self.secondary_site_menus = tuple(
-            self.create_test_menus_for_site(self.secondary_site, count=2)
+        self.not_default_site_menus = tuple(
+            self.create_test_menus_for_site(self.not_default_site)
         )
 
     def test_returns_none_if_no_match_for_supplied_site_and_fall_back_to_default_site_menus_is_false(self):
-        test_handle_value = 'test-3'
-        test_site_value = self.secondary_site
+        test_handle = 'test-1'
 
-        default_site = self.site
-        self.assertTrue(
-            FlatMenu.objects.filter(site=default_site, handle=test_handle_value).exists()
-        )
-        self.assertFalse(
-            FlatMenu.objects.filter(site=test_site_value, handle=test_handle_value).exists()
-        )
+        FlatMenu.objects.get(site=self.not_default_site, handle=test_handle).delete()
 
         with self.assertNumQueries(1):
             result = FlatMenu.get_for_site(
-                handle=test_handle_value,
-                site=test_site_value,
+                handle=test_handle,
+                site=self.not_default_site,
                 fall_back_to_default_site_menus=False
             )
         self.assertIs(result, None)
 
     def test_returns_menu_for_default_site_if_no_match_for_supplied_site_and_fall_back_to_default_site_menus_is_true(self):
-        test_handle_value = 'test-3'
-        test_site_value = self.secondary_site
+        test_handle = 'test-1'
 
-        default_site = Site.objects.get(is_default_site=True)
-        expected_result = FlatMenu.objects.get(site=default_site, handle=test_handle_value)
-        self.assertFalse(
-            FlatMenu.objects.filter(site=test_site_value, handle=test_handle_value).exists()
-        )
+        FlatMenu.objects.get(site=self.not_default_site, handle=test_handle).delete()
 
-        # Because the supplied site isn't the default site, get_default_site_id()
-        # will be first be called to identify the default site
-        with self.assertNumQueries(2):
+        expected_result = FlatMenu.objects.get(
+            site=self.default_site, handle=test_handle)
+
+        with self.assertNumQueries(1):
             result = FlatMenu.get_for_site(
-                handle=test_handle_value,
-                site=test_site_value,
+                handle=test_handle,
+                site=self.not_default_site,
                 fall_back_to_default_site_menus=True
             )
         self.assertEqual(result, expected_result)
 
-    def test_no_additional_queries_made_if_the_supplied_site_is_the_default_site(self):
-        test_handle_value = 'test-1'
-        default_site = self.site
-        self.assertTrue(default_site.is_default_site)
-
-        with self.assertNumQueries(1):
-            FlatMenu.get_for_site(
-                handle=test_handle_value,
-                site=default_site,
-                fall_back_to_default_site_menus=True
-            )
-
-        with self.assertNumQueries(1):
-            FlatMenu.get_for_site(
-                handle=test_handle_value,
-                site=default_site,
-                fall_back_to_default_site_menus=False
-            )
+    def test_returns_provided_site_matches_over_default_site_matches(self):
+        for handle in ('test-1', 'test-2', 'test-3'):
+            with self.assertNumQueries(1):
+                result = FlatMenu.get_for_site(
+                    handle=handle,
+                    site=self.not_default_site,
+                    fall_back_to_default_site_menus=True
+                )
+            self.assertEqual(result.site_id, self.not_default_site.id)
 
 
 class TestGetSubMenuTemplateNames(
