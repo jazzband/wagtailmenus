630802a14f7beb55c1180afe9647f8fe0973ad07
Andy Babic <ababic@rkh.co.uk>
2018-05-19 19:56:24 +0100

    Remove 'sphinx.ext.autodoc' from extensions for now

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,10 +34,7 @@ from filters import ContributorNamesFilter, MentionsFilter
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinxcontrib.spelling',
-    'sphinx.ext.autodoc',
-]
+extensions = ['sphinxcontrib.spelling']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
