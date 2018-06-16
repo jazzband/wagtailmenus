6ac1ae838a44ceee4d8054db0a7fb0d2cc42e9cd
Andy Babic <ababic@rkh.co.uk>
2018-06-16 16:30:38 +0100

    Cycle deprecation warnings

--- wagtailmenus/utils/deprecation.py
+++ wagtailmenus/utils/deprecation.py
@@ -1,13 +1,13 @@
-class RemovedInWagtailMenus211Warning(DeprecationWarning):
+class RemovedInWagtailMenus212Warning(DeprecationWarning):
     pass
 
 
-removed_in_next_version_warning = RemovedInWagtailMenus211Warning
-
-
-class RemovedInWagtailMenus212Warning(PendingDeprecationWarning):
-    pass
+removed_in_next_version_warning = RemovedInWagtailMenus212Warning
 
 
 class RemovedInWagtailMenus213Warning(PendingDeprecationWarning):
+    pass
+
+
+class RemovedInWagtailMenus214Warning(PendingDeprecationWarning):
     pass
