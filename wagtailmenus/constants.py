baeb7a1e09d3d2e25c04a8eaff3a6a829fde908a
Andy Babic <ababic@rkh.co.uk>
2018-09-10 23:48:40 +0100

    Cycle deprecation warnings and remove features that have reached the end of their deprecation period

--- wagtailmenus/constants.py
+++
@@ -1,9 +0,0 @@
-import warnings
-from wagtailmenus.conf.constants import * # noqa
-from wagtailmenus.utils.deprecation import RemovedInWagtailMenus212Warning
-
-warnings.warn(
-    "The 'wagtailmenus.constants' module is deprecated in favour of using "
-    "'wagtailmenus.conf.constants'. Please update the import statements in "
-    "your code to use the new path.", category=RemovedInWagtailMenus212Warning
-)
