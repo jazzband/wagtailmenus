d4cf1d15a7bff30d2e620180a8d9b8b7bb5a11b3
Andy Babic <ababic@rkh.co.uk>
2018-09-10 21:50:37 +0100

    Fix django-cogwheels to 0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ branch_url = "%stree/stable/%s" % (base_url, stable_branch_name)
 
 # Essential dependencies
 requires = [
-    'django-cogwheels',
+    'django-cogwheels==0.2',
 ]
 
 testing_extras = [
