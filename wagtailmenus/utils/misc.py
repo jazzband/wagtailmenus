1caee75b083a098f924d3abb0a3b28cb44ddb0a2
Andy Babic <ababic@rkh.co.uk>
2018-12-02 15:52:05 +0000

    Add a make_dummy_request() method to help with derive pages from 'current_url'

--- wagtailmenus/utils/misc.py
+++ wagtailmenus/utils/misc.py
@@ -1,3 +1,9 @@
+from io import StringIO
+from urllib.parse import urlparse
+
+from django.conf import settings
+from django.core.handlers.base import BaseHandler
+from django.core.handlers.wsgi import WSGIRequest
 from django.http import Http404, HttpRequest
 from wagtail.core.models import Page, Site
 
@@ -108,6 +114,62 @@ def derive_section_root(page):
         return page.specific
     if page.depth > desired_depth:
         return page.get_ancestors().get(depth=desired_depth).specific
+
+
+def make_dummy_request(url, original_request, **metadata):
+    """
+    Construct a HttpRequest object that is, as far as possible,
+    representative of the original request - only for the provided ``url``
+    instead of the one that was originally requested.
+    """
+    url_info = urlparse(url)
+    hostname = url_info.hostname
+    path = url_info.path
+    port = url_info.port or 80
+    scheme = url_info.scheme
+
+    dummy_values = {
+        'REQUEST_METHOD': 'GET',
+        'PATH_INFO': path,
+        'SERVER_NAME': hostname,
+        'SERVER_PORT': port,
+        'SERVER_PROTOCOL': 'HTTP/1.1',
+        'HTTP_HOST': hostname,
+        'wsgi.version': (1, 0),
+        'wsgi.input': StringIO(),
+        'wsgi.errors': StringIO(),
+        'wsgi.url_scheme': scheme,
+        'wsgi.multithread': True,
+        'wsgi.multiprocess': True,
+        'wsgi.run_once': False,
+    }
+
+    # Add important values from the original request object
+    headers_to_copy = [
+        'REMOTE_ADDR', 'HTTP_X_FORWARDED_FOR', 'HTTP_COOKIE',
+        'HTTP_USER_AGENT', 'HTTP_AUTHORIZATION', 'wsgi.version',
+        'wsgi.multithread', 'wsgi.multiprocess', 'wsgi.run_once',
+    ]
+    if settings.SECURE_PROXY_SSL_HEADER:
+        headers_to_copy.append(settings.SECURE_PROXY_SSL_HEADER[0])
+    for header in headers_to_copy:
+        if header in original_request.META:
+            dummy_values[header] = original_request.META[header]
+
+    # Add additional custom metadata sent by the caller.
+    dummy_values.update(**metadata)
+
+    request = WSGIRequest(dummy_values)
+
+    # Add a flag to let middleware know that this is a dummy request.
+    request.is_dummy = True
+
+    # Apply middleware to the request
+    handler = BaseHandler()
+    handler.load_middleware()
+    handler._middleware_chain(request)
+
+    return request
 
 
 def validate_supplied_values(tag, max_levels=None, parent_page=None,
