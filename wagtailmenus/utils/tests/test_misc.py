604d6fe92c1ef1a0c21c402decf3c8bb889247a7
Andy Babic <ababic@rkh.co.uk>
2018-12-14 22:00:33 +0000

    Remove pointless query here (we're mocking the method)

--- wagtailmenus/utils/tests/test_misc.py
+++ wagtailmenus/utils/tests/test_misc.py
@@ -294,8 +294,6 @@ class TestGetSiteFromRequest(TestCase):
     @mock.patch.object(Site, 'find_for_request', side_effect=Site.DoesNotExist())
     def test_returns_none_if_find_for_request_raises_doesnotexist_error(self, mocked_method):
         request = request_factory.get('/')
-        Site.objects.all().update(is_default_site=False)
-
         result = get_site_from_request(request)
         self.assertIs(result, None)
 
