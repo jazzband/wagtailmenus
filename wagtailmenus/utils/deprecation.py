cf7e443e96de6646ee4bf534bd95a8cef17977b5
Andy Babic <andy.babic@torchbox.com>
2020-06-28 22:10:45 +0100

    Bump version and cycle deprecation warnings

--- wagtailmenus/utils/deprecation.py
+++ wagtailmenus/utils/deprecation.py
@@ -1,12 +1,8 @@
-class RemovedInWagtailMenus31Warning(DeprecationWarning):
+class RemovedInWagtailMenus32Warning(DeprecationWarning):
     pass
 
 
-removed_in_next_version_warning = RemovedInWagtailMenus31Warning
-
-
-class RemovedInWagtailMenus32Warning(PendingDeprecationWarning):
-    pass
+removed_in_next_version_warning = RemovedInWagtailMenus32Warning
 
 
 class RemovedInWagtailMenus33Warning(PendingDeprecationWarning):
