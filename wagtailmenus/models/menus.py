1d8461f1a84031fd6845c6208994e68bf303961c
David Nugent <davidn@uniquode.io>
2020-11-05 10:25:40 +1100

    Replace ugettext_lazy with gettext_lazy
    Replace removed `ugettext_lazy` method with its alias `gettext_lazy`.
    
    Signed-off-by: Martey Dodoo <martey@mobolic.com>

--- wagtailmenus/models/menus.py
+++ wagtailmenus/models/menus.py
@@ -8,7 +8,7 @@ from django.http import HttpRequest
 from django.template.loader import get_template, select_template
 from django.utils.functional import cached_property, lazy
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from modelcluster.models import ClusterableModel
 from wagtail.core import hooks
 from wagtail.core.models import Page, Site
