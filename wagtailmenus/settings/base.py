62b28e70f75b8c10a81d3da81dd07a985d4aaa66
Andy Babic <andy.babic@torchbox.com>
2021-02-24 08:51:14 +0000

    Drop SiteMiddleware from settings.MIDDLEWARE

--- wagtailmenus/settings/base.py
+++ wagtailmenus/settings/base.py
@@ -88,8 +88,7 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'wagtail.core.middleware.SiteMiddleware',
-    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
+    'wagtail.contrib.redirects.middleware.RedirectMiddleware'
 )
 
 STATICFILES_FINDERS = (
