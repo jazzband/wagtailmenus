78e11fdcfe54f2af4caeb1a6286dbaec491d3537
Tobias Ernst <tobi@legartis.ai>
2022-03-19 09:59:45 +0100

    django4

--- wagtailmenus/forms.py
+++ wagtailmenus/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from wagtail.admin.forms import WagtailAdminModelForm, WagtailAdminPageForm
 
 from wagtailmenus.conf import settings
--- wagtailmenus/tests/urls.py
+++ wagtailmenus/tests/urls.py
@@ -1,21 +1,21 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import path
 from django.views.generic import TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 
-
 urlpatterns = [
-    url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'^custom-url/$', TemplateView.as_view(template_name='page.html')),
-    url(r'^sub_menu-tag-used-directly/$',
-        TemplateView.as_view(template_name='sub_menu-tag-used-directly.html')),
-    url(r'^superheroes/marvel-comics/custom-man/about/$',
-        TemplateView.as_view(template_name='page.html')),
-    url(r'^about-us/meet-the-team/staff-member-one/$',
-        TemplateView.as_view(template_name='page.html')),
-    url(r'^about-us/meet-the-team/custom-url/$',
-        TemplateView.as_view(template_name='page.html')),
-    url(r'^news-and-events/$',
-        TemplateView.as_view(template_name='page.html')),
-    url(r'', include(wagtail_urls)),
+    path('admin/', include(wagtailadmin_urls)),
+    path('custom-url/', TemplateView.as_view(template_name='page.html')),
+    path('sub_menu-tag-used-directly/',
+         TemplateView.as_view(template_name='sub_menu-tag-used-directly.html')),
+    path('superheroes/marvel-comics/custom-man/about/',
+         TemplateView.as_view(template_name='page.html')),
+    path('about-us/meet-the-team/staff-member-one/',
+         TemplateView.as_view(template_name='page.html')),
+    path('about-us/meet-the-team/custom-url/',
+         TemplateView.as_view(template_name='page.html')),
+    path('news-and-events/',
+         TemplateView.as_view(template_name='page.html')),
+    path('', include(wagtail_urls)),
 ]
