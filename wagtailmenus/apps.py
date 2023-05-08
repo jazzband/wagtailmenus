1beff20affcf83a35354d9383ec6e2e4cfb68da9
Fernando Cordeiro <fernando_cordeiro@live.com>
2023-05-08 14:19:22 +0100

    👌 IMPROVE: Set default_auto_field explicitly

--- wagtailmenus/apps.py
+++ wagtailmenus/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class WagtailMenusConfig(AppConfig):
     name = 'wagtailmenus'
     verbose_name = 'WagtailMenus'
+    default_auto_field = "django.db.models.AutoField"
