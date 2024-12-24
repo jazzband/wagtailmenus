9265efedacd36fd848017caf8967d5051b76d0d3
Nick Moreton <nickmoreton@me.com>
2024-12-24 11:13:00 +0000

    Make the regex less specific

--- wagtailmenus/tests/test_backend.py
+++ wagtailmenus/tests/test_backend.py
@@ -86,7 +86,7 @@ class CMSUsecaseTests(WebTest):
         form = edit_view.forms[next(reversed(OrderedDict(edit_view.forms)))]
         response = form.submit().follow()
 
-        assert "Main menu 'Main menu for wagtailmenus (co.uk)' updated." in response
+        assert re.search(r"Main menu .*? updated\.", response.content.decode())
 
 
 class LinkPageCMSTest(TestCase):
